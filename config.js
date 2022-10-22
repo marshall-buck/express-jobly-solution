f042ccaeb721bc77b26bb6c29c50fe2e0f80a090
Marshall Buck <dev@marshallbuck.com>
2022-10-22 18:21:16 -0500

    should be ready for deploy

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+    ? "jobly_test"
+    : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
@@ -21,12 +21,12 @@ function getDatabaseUri() {
 // WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-console.log("Jobly Config:".green);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
-console.log("PORT:".yellow, PORT.toString());
-console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
-console.log("---");
+// console.log("Jobly Config:".green);
+// console.log("SECRET_KEY:".yellow, SECRET_KEY);
+// console.log("PORT:".yellow, PORT.toString());
+// console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+// console.log("Database:".yellow, getDatabaseUri());
+// console.log("---");
 
 module.exports = {
   SECRET_KEY,
