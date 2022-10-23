c90e86e2c97517721e88252e71ccee199f38d6fc
Marshall Buck <dev@marshallbuck.com>
2022-10-22 20:31:44 -0500

    cahnged config

--- server.js
+++ server.js
@@ -3,6 +3,6 @@
 const app = require("./app");
 const { PORT, DOMAIN } = require("./config");
 
-app.listen(PORT, function () {
+app.listen(PORT, DOMAIN, function () {
   console.log(`Started on http://${DOMAIN}:${PORT}`);
 });;
